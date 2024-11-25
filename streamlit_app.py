261b627644f4f139c6a0ea6e4da96a325a4a1c8b
snowflakes-training <sitzvino@gmail.com>
2024-11-25 15:41:05 +0530

    Update streamlit_app.py
    connection changes need bc to move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -11,7 +11,8 @@ st.write(
     """
 )
 
-
+cnx = st.connection("snowflake")
+session = cnx.session()
 #option = st.selectbox(
  #   "What is your favourite fruit?",
   #  ("Banana", "Strawberries", "Peaches"),
